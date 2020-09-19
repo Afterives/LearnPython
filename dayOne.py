6250f22831509c2c314d62676246df7c6856c87b
Afterives <rogprzemek9@gmail.com>
2020-09-19 16:18:22 +0200

    Learn python - day 1

---
+++ dayOne.py
@@ -0,0 +1,22 @@
+# dzień pierwszy z Pythonem
+# zmienne i operacje na nich
+# String
+
+name = "Przemek"
+subname = "Nowak"
+print(name + " " + subname)
+
+another_string = name + " " + subname
+print(another_string)
+
+# int
+number = 4
+number2 = 7
+number3 = 10
+print(number + number2)
+print(number3 + number)
+
+# float
+float_number = 5.3
+float_number1 = 7.8
+print(float_number + float_number1)
